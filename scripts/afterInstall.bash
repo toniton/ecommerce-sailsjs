ee3245e52fe314b344ab105d0b7ef2f75c37ff57
Toniton <akinjiolatoni@gmail.com>
2018-01-22 10:01:57 +0100

    Code deployment config

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -3,8 +3,8 @@
 # I have left a few things in here and will explain this further (see below)
 #rsync --delete-before --verbose --archive --exclude "htdocs/media/" --exclude ".*" --exclude "htdocs/var/" --exclude "htdocs/app/etc/local.xml" /var/www/release/ /var/www/magento/ > /var/log/deploy.log
 
-sudo mkdir -vp /var/www/html/paytron/server/config/ssl/
-sudo cp -r /etc/ssl/paytron/* /var/www/html/paytron/server/config/ssl/
-sudo chown -R 1000 /var/www/html/paytron/server/.tmp
-sudo rm -rf /var/www/html/paytron/server/.tmp/public/
-cd /var/www/html/paytron/server && sudo npm i && forever stop forever/config.json && forever start forever/config.json
+sudo mkdir -vp /var/www/html/ecommerce/config/ssl/
+sudo cp -r /etc/ssl/paytron/* /var/www/html/ecommerce/config/ssl/
+sudo chown -R 1000 /var/www/html/ecommerce/.tmp
+sudo rm -rf /var/www/html/ecommerce/.tmp/public/
+cd /var/www/html/ecommerce && sudo npm i && sudo su && pm2 startOrRestart pm2.json --env production --watch
--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -3,9 +3,9 @@
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
 # exist.
-cd /var/www/html/paytron/server
-forever stop app.js
-if [ -d /var/www/html/paytron/server/ ]; then
-    rm -rf /var/www/html/paytron/server/
+cd /var/www/html/ecommerce
+pm2 stop pm2.json
+if [ -d /var/www/html/ecommerce/ ]; then
+    rm -rf /var/www/html/ecommerce/
 fi
-mkdir -vp /var/www/html/paytron/server/
+mkdir -vp /var/www/html/ecommerce/
