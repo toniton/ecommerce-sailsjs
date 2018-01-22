44cf57f9bc8d62a867a2b5c0377f75bf034402ad
Toniton <akinjiolatoni@gmail.com>
2018-01-22 20:57:38 +0100

    Rectified apspec error

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -3,8 +3,8 @@
 # I have left a few things in here and will explain this further (see below)
 #rsync --delete-before --verbose --archive --exclude "htdocs/media/" --exclude ".*" --exclude "htdocs/var/" --exclude "htdocs/app/etc/local.xml" /var/www/release/ /var/www/magento/ > /var/log/deploy.log
 
-sudo mkdir -vp /var/www/html/ecommerce/config/ssl/
-sudo cp -r /etc/ssl/paytron/* /var/www/html/ecommerce/config/ssl/
+#sudo mkdir -vp /var/www/html/ecommerce/config/ssl/
+#sudo cp -r /etc/ssl/ecommerce/* /var/www/html/ecommerce/config/ssl/
 
 if [ -d /var/www/html/ecommerce/.tmp ]; then
     sudo chown -R 1000 /var/www/html/ecommerce/.tmp/
